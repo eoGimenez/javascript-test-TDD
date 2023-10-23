6acebaf402e9e0a55accbe01da436a366f7681d7
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-10-23 23:01:08 +0200

    refactorizado de funcion

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,9 +1,12 @@
 export const fizzbuzz = (number) => {
 	if (typeof number !== 'number' || isNaN(number)) throw new Error('Must provide a valid number.')
 
-	if (number % 15 === 0) return 'fizzbuzz'
-	if (number % 3 === 0) return 'fizz'
-	if (number % 5 === 0) return 'buzz'
+	const multiplies = { 3: 'fizz', 5: 'buzz' }
+	let output = ''
 
-	return number
+	Object.entries(multiplies).forEach(([multiplier, word]) => {
+		if (number % multiplier === 0) output += word
+	})
+
+	return output === '' ? number : output
 }
