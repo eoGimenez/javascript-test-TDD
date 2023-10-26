71e47bb3dc388166b2011651bde19cf5ec24c4f1
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-10-26 20:26:52 +0200

    testeando eventos

--- test/react.test.jsx
+++ test/react.test.jsx
@@ -1,22 +1,40 @@
-import { cleanup, render, screen } from '@testing-library/react'
+import { cleanup, fireEvent, render, screen } from '@testing-library/react'
+import { useState } from 'react'
 import { afterEach, describe, expect, it } from 'vitest'
 
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 const rows = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [0]]
 
+const operations = ['+', '-', '*', '/']
+
+const equalSign = '='
+
 const Calculator = () => {
+	const [value, setValue] = useState('')
 	return (
 		<section>
 			<h1>Calculator</h1>
+			<input value={value} readOnly />
 			<div role='grid'>
 				{rows.map((row, idx) => (
 					<div key={idx} role='row'>
 						{row.map((num) => (
-							<span key={num}>{num}</span>
+							<button
+								key={num}
+								onClick={() => {
+									setValue(num)
+								}}
+							>
+								{num}
+							</button>
 						))}
 					</div>
 				))}
+				{operations.map((operation) => (
+					<span key={operation}>{operation}</span>
+				))}
+				<span>{equalSign}</span>
 			</div>
 		</section>
 	)
@@ -35,12 +53,44 @@ describe('Calculator', () => {
 	})
 	it('Should render numbers', () => {
 		render(<Calculator />)
+
 		numbers.forEach((num) => screen.getByText(num))
 	})
 
 	it('Should render 4 rows', () => {
 		render(<Calculator />)
+
 		const rows = screen.getAllByRole('row')
-		expect(rows).toHaveLength(4)
+		expect(rows.length).toBe(4)
+	})
+
+	it('Should render operations', () => {
+		render(<Calculator />)
+
+		operations.forEach((operation) => {
+			screen.getByText(operation)
+		})
+	})
+
+	it('Shoudl render "="', () => {
+		render(<Calculator />)
+
+		screen.getByText('=')
+	})
+
+	it('Should render an input', () => {
+		render(<Calculator />)
+
+		screen.getByRole('textbox')
+	})
+
+	it('Should user input after clicking a number', () => {
+		render(<Calculator />)
+
+		const one = screen.getByText('1')
+		fireEvent.click(one)
+
+		const input = screen.getByRole('textbox')
+		expect(input.value).toBe('1')
 	})
 })
